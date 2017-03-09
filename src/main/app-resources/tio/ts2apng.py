7a0a7fed021725735b801cb9a054255b4b3608fe
Matthieu Volat <matthieu.volat@univ-grenoble-alpes.fr>
2017-03-09 19:49:46 +0100

    Update timeseries quicklook script.

--- src/main/app-resources/tio/ts2apng.py
+++ src/main/app-resources/tio/ts2apng.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>
- 
 from __future__ import print_function
 import binascii, os, sys, struct, subprocess, warnings
 warnings.simplefilter("ignore", category=RuntimeWarning)
@@ -48,23 +35,23 @@ for i in range(ds.RasterCount):
 # TODO: would be cool to generate extra images to smoothe transitions
 # Use a matplotlib figure to colorize
 fig = plt.figure(frameon=False)
-fig.set_size_inches(ds.RasterXSize/72, ds.RasterYSize/72)
+fig.set_size_inches(ds.RasterXSize/4/72, ds.RasterYSize/4/72)
 ax = plt.Axes(fig, [0., 0., 1., 1.])
 ax.set_axis_off()
 fig.add_axes(ax)
 for i in range(ds.RasterCount):
     band = ds.GetRasterBand(i+1)
-    data = band.ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize)
+    data = band.ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize, ds.RasterXSize/4, ds.RasterYSize/4)
     data[data == band.GetNoDataValue()] = np.nan
     plt.imshow(data, cmap="jet", interpolation="bilinear", vmin=dsmin, vmax=dsmax)
     fig.savefig("quicklook_tmp_%03d.png" % i, dpi=72)
 
 # Convert to APNG
-status = subprocess.call("/home/mvolat/ffmpeg -y -loglevel panic -framerate 4 -i 'quicklook_tmp_%%03d.png' -f apng -c:v apng %s.png" % sys.argv[1], shell=True)
+status = subprocess.call("/home/mvolat/ffmpeg -y -loglevel panic -framerate 4 -i 'quicklook_tmp_%%03d.png' -f apng -c:v apng %s.png" % os.path.splitext(sys.argv[1])[0], shell=True)
 
 # ffmpeg do not allow (yet) to set APNG loop attribute like it does with GIF...
 # so do it the hard way: hack into the file :)
-apngf = open("%s.png" % sys.argv[1], "r+b")
+apngf = open("%s.png" % os.path.splitext(sys.argv[1])[0], "r+b")
 apngf.read(8)
 while(True):
     chunk_len = struct.unpack(">I", apngf.read(4))[0]
