255ae5c241f287bb2366a52b2ff249fdd3e88ec3
Matthieu Volat <matthieu.volat@univ-grenoble-alpes.fr>
2017-03-10 16:56:00 +0100

    Reformat RMSpixel files into single tiffs.

--- src/main/app-resources/tio/run.sh
+++ src/main/app-resources/tio/run.sh
@@ -56,8 +56,8 @@ cd $OLDPWD
 # Create input files
 ciop-log "INFO" "Create invers_pixel input files"
 
-couples=$(ls -1 LN_DATA/*.r4 | sed 's/\.r4//' | xargs -L1 basename)
-dates=$(echo "$couples" | tr - \\n | sort -u)
+pairs=$(ls -1 LN_DATA/*.r4 | sed 's/\.r4//' | xargs -L1 basename)
+dates=$(echo "$pairs" | tr - \\n | sort -u)
 
 # create liste_image_inv file
 date0=$(echo $dates | head -n1)
@@ -68,14 +68,14 @@ for date in $dates; do
 	printf '%d %f %f %d\n' $date $date_float $date_diff 0 >> liste_image_inv
 done
 
-# create liste_couple file
-for couple in $couples; do
-	date1=$(echo $couple|cut -d- -f1)
-	date2=$(echo $couple|cut -d- -f2)
+# create liste_pair file
+for pair in $pairs; do
+	date1=$(echo $pair|cut -d- -f1)
+	date2=$(echo $pair|cut -d- -f2)
 	date1_float=$(echo "scale=6; $(echo $date1|cut -c1-4) + ($(echo $date1|cut -c5-6)-1)/12 + ($(echo $date1|cut -c7-8)-1)/365" | bc)
 	date2_float=$(echo "scale=6; $(echo $date2|cut -c1-4) + ($(echo $date2|cut -c5-6)-1)/12 + ($(echo $date2|cut -c7-8)-1)/365" | bc)
 	coeff=$(echo "scale=6; 1 / (1 + ($date2_float-$date1_float)^2)^2" | bc)
-    printf '%s %s %f\n' $date1 $date2 $coeff >> liste_couple
+    printf '%s %s %f\n' $date1 $date2 $coeff >> liste_pair
 done
 
 # create invers_pixel_param file
@@ -92,7 +92,7 @@ $(ciop-getparam mask_large_residuals) % iterations to mask (tiny weight) pixels 
 1    %   outliers elimination by the median (only if nsamp>1) ? (y=0,n=1)
 liste_image_inv
 0    % sort by date (0) ou by another variable (1) ?
-liste_couple
+liste_pair
 1   % interferogram format (RMG : 0; R4 :1) (date1-date2_pre_inv.unw or date1-date2.r4)
 3100.   %  include interferograms with bperp lower than maximal baseline
 1   %   minimal number of interferams using each image
@@ -114,16 +114,101 @@ EOF
 # run invers_pixel
 ciop-log "INFO" "Calling invers_pixel"
 /home/mvolat/timeseries/invers_pixel invers_pixel_param || exit $ERR_INVERS_PIXEL
+gdalinfo -stats depl_cumule &>/dev/null # force .aux.xml creation
+gdalinfo -stats depl_cumule_liss &>/dev/null # force .aux.xml creation
+depl_cumule_info=$(gdalinfo -nomd -norat -noct depl_cumule)
+depl_cumule_xsize=$(printf "$depl_cumule_info" | grep "^Size is " | tr -d , | cut -d' ' -f3)
+depl_cumule_ysize=$(printf "$depl_cumule_info" | grep "^Size is " | tr -d , | cut -d' ' -f4)
+depl_cumule_bands=$(printf "$depl_cumule_info" | grep "^Band " | wc -l)
 
 # run lect_depl_cumule_lin
 ciop-log "INFO" "Calling lect_depl_cumule_lin"
-depl_cumule_info=$(gdalinfo -nomd -norat -noct depl_cumule)
 /home/mvolat/timeseries/lect_depl_cumule_lin \
-	$(printf "$depl_cumule_info" | grep "^Size is " | tr -d , | cut -d' ' -f3) \
-	$(printf "$depl_cumule_info" | grep "^Size is " | tr -d , | cut -d' ' -f4) \
-	$(printf "$depl_cumule_info" | grep "^Band " | wc -l) \
+	$depl_cumule_xsize \
+	$depl_cumule_ysize \
+	$depl_cumule_bands \
     1 \
     1
+
+# reformat output into tiff
+ciop-log "INFO" "Reformat output"
+# depl_cumule_* files, easy
+gdal_translate -q -co "INTERLEAVE=BAND" -co "COMPRESS=DEFLATE" -co "PREDICTOR=3" depl_cumule depl_cumule_${direction}.tiff
+cp depl_cumule.aux.xml depl_cumule_${direction}.tiff.aux.xml
+gdal_translate -q -co "INTERLEAVE=BAND" -co "COMPRESS=DEFLATE" -co "PREDICTOR=3" depl_cumule_liss depl_cumule_liss_${direction}.tiff
+cp depl_cumule_liss.aux.xml depl_cumule_liss_${direction}.tiff.aux.xml
+
+# create vrt for RMSpixel files per date
+cat > RMSpixel_dates.vrt << EOF
+<VRTDataset rasterXSize="$depl_cumule_xsize" rasterYSize="$depl_cumule_ysize">
+EOF
+for date in $dates; do
+	f=RMSpixel_$date
+	cat > ${f}.hdr << EOF
+ENVI
+samples = $depl_cumule_xsize
+lines = $depl_cumule_ysize
+bands = 1
+header offset = 0
+data type = 4
+interleave = bip
+byte order = 0
+EOF
+	cat >> RMSpixel_dates.vrt << EOF
+  <VRTRasterBand dataType="Float32">
+    <SimpleSource>
+      <SourceFilename relativeToVRT="1">$f</SourceFilename>
+      <SourceBand>1</SourceBand>
+      <SourceProperties RasterXSize="$depl_cumule_xsize" RasterYSize="$depl_cumule_ysize" DataType="Float32" BlockXSize="$depl_cumule_xsize" BlockYSize="1" />
+      <SrcRect xOff="0" yOff="0" xSize="$depl_cumule_xsize" ySize="$depl_cumule_ysize" />
+      <DstRect xOff="0" yOff="0" xSize="$depl_cumule_xsize" ySize="$depl_cumule_ysize" />
+      <Description>$date</Description>
+    </SimpleSource>
+  </VRTRasterBand>
+EOF
+done
+cat >> RMSpixel_dates.vrt << EOF
+</VRTDataset>
+EOF
+# pack all RMSpixel files per date into a single tiff
+gdal_translate -q -co "INTERLEAVE=BAND" -co "COMPRESS=DEFLATE" -co "PREDICTOR=3" RMSpixel_dates.vrt RMSpixel_dates_${direction}.tiff
+
+# create vrt for RMSpixel files per pair
+cat > RMSpixel_pairs.vrt << EOF
+<VRTDataset rasterXSize="$depl_cumule_xsize" rasterYSize="$depl_cumule_ysize">
+EOF
+for pair in $pairs; do
+	date1=$(echo $pair|cut -d- -f1)
+	date2=$(echo $pair|cut -d- -f2)
+	f=RMSpixel_${date1}_${date2}
+	cat > ${f}.hdr << EOF
+ENVI
+samples = $depl_cumule_xsize
+lines = $depl_cumule_ysize
+bands = 1
+header offset = 0
+data type = 4
+interleave = bip
+byte order = 0
+EOF
+	cat >> RMSpixel_pairs.vrt << EOF
+  <VRTRasterBand dataType="Float32">
+    <SimpleSource>
+      <SourceFilename relativeToVRT="1">$f</SourceFilename>
+      <SourceBand>1</SourceBand>
+      <SourceProperties RasterXSize="$depl_cumule_xsize" RasterYSize="$depl_cumule_ysize" DataType="Float32" BlockXSize="$depl_cumule_xsize" BlockYSize="1" />
+      <SrcRect xOff="0" yOff="0" xSize="$depl_cumule_xsize" ySize="$depl_cumule_ysize" />
+      <DstRect xOff="0" yOff="0" xSize="$depl_cumule_xsize" ySize="$depl_cumule_ysize" />
+      <Description>${date1}-${date2}</Description>
+    </SimpleSource>
+  </VRTRasterBand>
+EOF
+done
+cat >> RMSpixel_pairs.vrt << EOF
+</VRTDataset>
+EOF
+# pack all RMSpixel files per date into a single tiff
+gdal_translate -q -co "INTERLEAVE=BAND" -co "COMPRESS=DEFLATE" -co "PREDICTOR=3" RMSpixel_pairs.vrt RMSpixel_pairs_${direction}.tiff
 
 # quicklook
 ciop-log "INFO" "Create quicklooks"
@@ -148,21 +233,16 @@ cat > depl_cumule_liss_${direction}.pngw <<EOF
 -16.262223407352135
 EOF
 
-# compress output
-ciop-log "INFO" "Reformat output"
-gdal_translate -q -co "INTERLEAVE=BAND" -co "COMPRESS=DEFLATE" -co "PREDICTOR=3" depl_cumule depl_cumule_${direction}.tiff
-gdal_translate -q -co "INTERLEAVE=BAND" -co "COMPRESS=DEFLATE" -co "PREDICTOR=3" depl_cumule_liss depl_cumule_liss_${direction}.tiff
-
 # clean
-ciop-log "INFO" "Clean directory before archiving"
-rm depl_cumule depl_cumule.hdr
-rm depl_cumule_liss depl_cumule_liss.hdr
-rm -Rf LN_DATA
-rm RMS*
-
-# Push results
+#ciop-log "INFO" "Clean directory before archiving"
+#rm -Rf LN_DATA
+#rm depl_cumule depl_cumule.hdr depl_cumule.aux.xml
+#rm depl_cumule_liss depl_cumule_liss.hdr depl_cumule_liss.aux.xml
+#rm RMSpixel_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9] RMSpixel_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].hdr
+#rm RMSpixel_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9] RMSpixel_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].hdr
+# tar
 #ciop-log "INFO" "Create archive file"
-#tar -C $(dirname $TMPDIR) -cjf /tmp/foobar/workdir.tar.bz2 $(basename $TMPDIR)
+#tar -C $(dirname $TMPDIR) -cjf /tmp/foobar/workdir_${direction}.tar.bz2 $(basename $TMPDIR)
 
 # Push results
 ciop-log "INFO" "Publishing png files"
